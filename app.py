55701411c53a49b368522d545f9180ff4948bf53
YgorLourenco <44757992+YgorLourenco@users.noreply.github.com>
2021-10-17 22:02:17 -0300

    Atualização

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import flask
+from flask import Flask
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<center><h1>Hello, World</h1></center>"
+    return "<center><h1>Hello, Flask!</h1></center>"
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host='0.0.0.0')
